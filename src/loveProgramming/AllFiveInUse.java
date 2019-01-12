94ec90aa79c4b08683bab99ff309b55025ac2122
archana sudani <archanasudani@gmail.com>
2019-01-12 17:33:39 +0000

    My First Commit

---
+++ src/loveProgramming/AllFiveInUse.java
@@ -0,0 +1,10 @@
+package loveProgramming;
+
+public class AllFiveInUse {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+    }
+
+}
---
+++ src/loveProgramming/Four.java
@@ -0,0 +1,17 @@
+package loveProgramming;
+
+public class Four {
+    public int getCountDown() {
+        int countDown = 10;
+            while(countDown>0){
+            countDown--;
+//            System.out.println("Countdown: " + countDown--);
+            ;
+        }
+        return countDown;
+    }
+
+
+
+}
+}
